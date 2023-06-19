d5885d651f0831673c911847ebe44eef6fe4c385
Shivam <singhshivamkr@gmail.com>
2023-06-19 18:12:33 +0530

    Initial commit

---
+++ src/DotnetCdkHttpApi/DotnetCdkHttpApiStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace DotnetCdkHttpApi
+{
+    public class DotnetCdkHttpApiStack : Stack
+    {
+        internal DotnetCdkHttpApiStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
